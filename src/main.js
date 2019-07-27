a47b997d8116c5fd1dc962519a14dcd7c02bdcfd
GokiSun <35653229+GokiSun@users.noreply.github.com>
2019-07-28 00:18:34 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import echarts from 'echarts'
 import store from './components/myVuex/vuex'
-Vue.prototype.$echarts = echarts
 import VueClipboard from 'vue-clipboard2'
 import echarts from 'echarts'
 
